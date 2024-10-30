eb6d28c54d86e00dac734b700e2e260017cc74ff
elenakhoo <elenakhoosk@gmail.com>
2024-10-31 04:01:25 +0800

    Update Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -33,7 +33,7 @@ const Cart = ({ cart, setCart, account }) => {
   const [isSaving, setIsSaving] = useState(false); // State to track if the save process is ongoing
   const [isEditVisible, setIsEditVisible] = useState(false); // Toggle visibility for edit section
 
-  const contractAddress = '0x6d925938edb8a16b3035a4cf34faa090f490202a'; // Replace with your deployed contract address
+  const contractAddress = '0x4A813F566f743A1a5936a1ac5C523E0b2F34F533'; // Replace with your deployed contract address
 
   const fetchShippingAddress = useCallback(async () => {
     if (account && window.ethereum) {
